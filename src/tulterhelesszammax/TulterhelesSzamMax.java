f86b126bf4350e6b62ca86c82199870b34b5429c
PatrikB <bekpatrik24@gmail.com>
2024-06-15 13:38:02 +0200

    TesztelesMegirva

--- src/tulterhelesszammax/TulterhelesSzamMax.java
+++ src/tulterhelesszammax/TulterhelesSzamMax.java
@@ -18,10 +18,7 @@ public class TulterhelesSzamMax {
         }
         return max;
     }
-        
-        
-        
-      
+       
     }
     
 
--- src/tulterhelesszammax/teszt.java
+++ src/tulterhelesszammax/teszt.java
@@ -3,5 +3,16 @@ package tulterhelesszammax;
 
 
 public class teszt {
-    
-}
+   
+         public static void main(String[] args) {
+        tesztSzamMax();
+    }
+
+    public static void tesztSzamMax() {
+        int[] szamok1 = {1, 2, 3, 4, 5, 8, 6};
+        int eredmeny = TulterhelesSzamMax.szamMax(szamok1);
+        assert eredmeny == 5 : "A szamMax metódus helytelen eredményt adott vissza!";
+        System.out.printf("A legnagyobb szám %d",eredmeny);
+    }
+    }    
+
