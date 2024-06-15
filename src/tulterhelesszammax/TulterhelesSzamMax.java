b5476b33ec7bdf48ab0c3c79245dd810e7d7967b
PatrikB <bekpatrik24@gmail.com>
2024-06-15 13:21:26 +0200

    MaxSzamCiklus

--- src/tulterhelesszammax/TulterhelesSzamMax.java
+++ src/tulterhelesszammax/TulterhelesSzamMax.java
@@ -6,8 +6,17 @@ public class TulterhelesSzamMax {
 
   
     public static void main(String[] args) {
-     
         
+    }
+
+    public static int szamMax(int [] szamok) {
+        int max = szamok[0];
+        for (int i = 0; i < szamok.length; i++) {
+            if(szamok[i] > max){
+            max = szamok[i];
+            }
+        }
+        return max;
     }
         
         
